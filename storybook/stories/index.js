0b96b374c8ba7f2cd2a13b16c243c8a0787abe31
JuliaRechkunova <julia.rechkunova@gmail.com>
2020-01-01 13:19:50 +0100

    Change Storybook import

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,19 +1,2 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react-native';
-import { action } from '@storybook/addon-actions';
-
-import Button from '../../src/components/Button';
-import CenterView from '../components/CenterView';
-import Welcome from '../components/Welcome';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome />);
-
-storiesOf('Button', module)
-  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-  .add('default', () => (
-    <Button label="Button" onPress={action('clicked-text')} />
-  ))
-  .add('with "+" icon', () => (
-    <Button icon="+" title="Button" onPress={action('clicked-text')} />
-  ));
+import './welcome';
+import './button';
