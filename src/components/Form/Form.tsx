5a8cb9393676fb285c1849106969a63d6add032f
JuliaRechkunova <julia.rechkunova@gmail.com>
2020-02-19 21:19:16 +0100

    Refactor more

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Button from '../Button/Button';
@@ -13,83 +13,61 @@ interface IProps {
   onSubmit: (values: FormValues) => Promise<void>;
 }
 
-interface IState {
-  values: FormValues;
-  status: FormSubmittingStatus;
+function getInitialValues(fields: FormField[]): FormValues {
+  return fields.reduce<FormValues>((values, field) => {
+    values[field.name] = field.defaultValue;
+    return values;
+  }, {});
 }
 
-class Form extends React.Component<IProps, IState> {
-  state = {
-    values: this.props.fields.reduce<FormValues>((values, field) => {
-      values[field.name] = field.defaultValue;
-      return values;
-    }, {}),
-    status: FormSubmittingStatus.initial,
-  };
+const Form = ({ fields, onSubmit }: IProps) => {
+  const [values, setValues] = useState(getInitialValues(fields));
+  const [status, setStatus] = useState(FormSubmittingStatus.initial);
 
-  handleChange = (value: string, field: FormField) => {
-    const { values } = this.state;
-
+  const handleChange = (value: string, field: FormField) => {
     if (values[field.name] !== value) {
-      this.setState((state: IState) => {
-        const newValues: FormValues = { ...state.values };
-        newValues[field.name] = value;
-
-        return {
-          values: newValues,
-        };
+      setValues({
+        ...values,
+        [field.name]: value,
       });
     }
   };
 
-  handleSubmit = async () => {
-    const { onSubmit } = this.props;
-    const { values } = this.state;
-
-    this.setState({
-      status: FormSubmittingStatus.processing,
-    });
+  const handleSubmit = async () => {
+    setStatus(FormSubmittingStatus.processing);
 
     try {
       await onSubmit(values);
 
-      this.setState({
-        status: FormSubmittingStatus.succeeded,
-      });
+      setStatus(FormSubmittingStatus.succeeded);
     } catch (error) {
-      this.setState({
-        status: FormSubmittingStatus.failed,
-      });
+      setStatus(FormSubmittingStatus.failed);
     }
   };
 
-  render() {
-    const { fields } = this.props;
-    const { values, status } = this.state;
-    const isSubmitting = status === FormSubmittingStatus.processing;
+  const isSubmitting = status === FormSubmittingStatus.processing;
 
-    return (
-      <View style={styles.container}>
-        {fields.map(field => (
-          <View style={styles.field} key={field.name}>
-            <FormInput
-              field={field}
-              value={values[field.name]}
-              onChange={this.handleChange}
-            />
-          </View>
-        ))}
-        <View style={styles.action}>
-          <Button
-            title={isSubmitting ? 'Submitting...' : 'Submit'}
-            disabled={isSubmitting || Object.keys(values).some(k => !values[k])}
-            onPress={this.handleSubmit}
+  return (
+    <View style={styles.container}>
+      {fields.map(field => (
+        <View style={styles.field} key={field.name}>
+          <FormInput
+            field={field}
+            value={values[field.name]}
+            onChange={handleChange}
           />
         </View>
+      ))}
+      <View style={styles.action}>
+        <Button
+          title={isSubmitting ? 'Submitting...' : 'Submit'}
+          disabled={isSubmitting || Object.keys(values).some(k => !values[k])}
+          onPress={handleSubmit}
+        />
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
