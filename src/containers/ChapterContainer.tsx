9270bc4a73134b2b4656eb43c785d1615ff89689
JuliaRechkunova <julia.rechkunova@gmail.com>
2020-02-23 13:49:21 +0100

    Use selector from redux

--- src/containers/ChapterContainer.tsx
+++ src/containers/ChapterContainer.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import { ActivityIndicator } from 'react-native';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavigationStackProp } from 'react-navigation-stack';
 
 import Blank from '../components/Blank/Blank';
 import CenterView from '../components/CenterView';
+import ChapterHeader from '../components/ChapterHeader/ChapterHeader';
 import ChapterItemsContainer from '../containers/ChapterItemsContainer';
 
 import { fetchChapter, resetChapter } from '../reducers/chapterSlice';
@@ -16,16 +17,12 @@ import {
   NavigationParamsChapter,
   NavigationParamsChapterForm,
 } from '../types/navigation.type';
-import { RootState, ThunkDispatch } from '../types/redux.type';
-import { INullableChapter, IChapter } from '../types/chapter.type';
-import ChapterHeader from '../components/ChapterHeader/ChapterHeader';
+import { RootState } from '../types/redux.type';
+import { IChapter } from '../types/chapter.type';
 
 interface IProps {
   chapterId: number;
-  chapter: INullableChapter;
-  loadingStatus: LoadingStatus;
   navigation: NavigationStackProp<NavigationParamsChapter>;
-  dispatch: ThunkDispatch;
 }
 
 function navigateToEditScreen(
@@ -44,50 +41,46 @@ function navigateToEditScreen(
   navigation.navigate(Screens.ChapterForm, params);
 }
 
-const ChapterContainer = React.memo<IProps>(
-  ({ chapterId, chapter, loadingStatus, navigation, dispatch }) => {
-    useEffect(() => {
-      dispatch(fetchChapter(chapterId));
+const ChapterContainer = React.memo<IProps>(({ chapterId, navigation }) => {
+  const dispatch = useDispatch();
+  const loadingStatus = useSelector(
+    (state: RootState) => state.chapter.loadingStatus,
+  );
+  const chapter = useSelector((state: RootState) => state.chapter.chapter);
 
-      return () => {
-        dispatch(resetChapter());
-      };
-    }, [chapterId, dispatch]);
+  useEffect(() => {
+    dispatch(fetchChapter(chapterId));
 
-    if (loadingStatus === LoadingStatus.failed) {
-      return (
-        <CenterView>
-          <Blank message="Chapter not found" />
-        </CenterView>
-      );
-    }
+    return () => {
+      dispatch(resetChapter());
+    };
+  }, [chapterId, dispatch]);
 
-    if (loadingStatus === LoadingStatus.loaded && chapter) {
-      return (
-        <React.Fragment>
-          <ChapterHeader
-            chapter={chapter}
-            onEdit={() => navigateToEditScreen(navigation, chapter)}
-          />
-          <ChapterItemsContainer
-            chapterId={chapter.id}
-            navigation={navigation}
-          />
-        </React.Fragment>
-      );
-    }
-
+  if (loadingStatus === LoadingStatus.failed) {
     return (
       <CenterView>
-        <ActivityIndicator />
+        <Blank message="Chapter not found" />
       </CenterView>
     );
-  },
-);
+  }
 
-const mapStateToProps = ({ chapter }: RootState) => ({
-  chapter: chapter.chapter,
-  loadingStatus: chapter.loadingStatus,
+  if (loadingStatus === LoadingStatus.loaded && chapter) {
+    return (
+      <React.Fragment>
+        <ChapterHeader
+          chapter={chapter}
+          onEdit={() => navigateToEditScreen(navigation, chapter)}
+        />
+        <ChapterItemsContainer chapterId={chapter.id} navigation={navigation} />
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <CenterView>
+      <ActivityIndicator />
+    </CenterView>
+  );
 });
 
-export default connect(mapStateToProps)(ChapterContainer);
+export default ChapterContainer;
